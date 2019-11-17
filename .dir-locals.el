9d88ef3198d461e0037f8d54c754af2a0e20e93a
Thiago Lira <thlira15@gmail.com>
2019-11-17 17:30:51 -0300

    take that emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,8 @@
-((nil . ((compile-command . latexmk)
-         )))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      (
+       (compilation-read-command)
+       (projectile-project-compilation-cmd . "latexmk")))
+ )
