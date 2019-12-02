63ae1ed0925560b5dc7d164ed9fc94792fc0c23c
ThiagoLira <thlira15@gmail.com>
2019-12-02 14:18:45 -0300

    tiny teeny changes

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,2 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-
-((nil .
-      (
-       (compilation-read-command)
-       (projectile-project-compilation-cmd . "latexmk")))
- )
